a7b4809bffc248c0939593344fc37f6458db5e8d
Naveen-nexus <nk4451@srmist.edu.in>
2026-03-14 00:03:39 +0530

    feat: add application routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const jobRoutes = require('./routes/jobRoutes');
+const applicationRoutes = require('./routes/applicationRoutes');
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
+app.use('/api/applications', applicationRoutes);
 
 // Base route
 app.get('/', (req, res) => {
